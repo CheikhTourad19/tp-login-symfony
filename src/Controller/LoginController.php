1da028e4702a3e9f76a0837db8da0ccc913046e4
cheikh tourad <mohamed.mohamedvadelcheikhtourad@ucad.edu.sn>
2025-02-15 15:35:32 +0100

    jçiàjà

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/', name: 'app_login')]
+    public function index(): Response
+    {
+        return $this->render('login/index.html.twig', [
+            'erreur' => ''
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login-check',methods: ['POST'])]
+    public function login(Request $request,UserRepository $userRepository):Response
+    {
+        $email = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        $user= $userRepository->findOneBy(['email'=>$email,'password'=>$password]);
+
+        if(!$user){
+            $this->addFlash('error', 'Identifiants incorrects');
+            return $this->redirectToRoute('app_login');
+        }
+        else{
+            return $this->redirectToRoute('app_home',['user'=>$user]);
+        }
+
+    }
+}
---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function index(): Response
+    {
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+        ]);
+    }
+
+    #[Route('/signup', name: 'app_signup')]
+    public function signup(Request $request, UserRepository $userRepository,EntityManagerInterface $entityManager): Response
+    {
+        $email = $request->request->get('email');
+        $nom= $request->request->get('nom');
+        $prenom= $request->request->get('prenom');
+        $password = $request->request->get('password');
+        $confirmPassword = $request->request->get('confirm_password');
+
+
+        // Vérification si les mots de passe correspondent
+        if ($password !== $confirmPassword) {
+            $this->addFlash('error', 'Les mots de passe ne correspondent pas');
+            return $this->redirectToRoute('app_signup');
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setNom($nom);
+        $user->setPrenom($prenom);
+        $user->setPassword($password);  // Ici on ne fait pas de hashage, c'est juste pour simplification
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        // Message de succès
+        $this->addFlash('success', 'Compte créé avec succès !');
+
+        // Redirection vers la page de connexion
+        return $this->redirectToRoute('app_register');
+    }
+}
